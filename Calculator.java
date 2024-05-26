70b0451d0ad727ddf38ab5ee95858f18e4a97c87
yesauthor <166253636+yesauthor@users.noreply.github.com>
2024-05-26 17:35:30 -0400

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-
 public class Calculator {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int reset = 0;
+		double reset = 0;
 		
 		while (reset == 0) {
 		reset = 1;
@@ -17,7 +16,9 @@ public class Calculator {
 				+ "\nType 6 to calculate the slope of the line between two points,"
 				+ "\nType 7 to calculate exponents, exponential functions and logarithms,"
 				+ "\nType 8 to calculate Pythagorean Theorem,"
-				+ "\nType 9 to calculate tangent, sine, and cosine (and their opposites):");
+				+ "\nType 9 to calculate tangent, sine, and cosine (and their opposites),"
+				+ "\nType 10 to calculate quadratic equations,"
+				+ "\nType 11 to calculate compounded interest formulas:");
 		
 		int decision = input.nextInt();
 		
@@ -41,64 +42,126 @@ public class Calculator {
 		
 		else if (decision == 2) {
 		
-		System.out.println("Please enter your first number: ");
-		double num1 = input.nextDouble();
+		System.out.println("Type 1 to do addition,"
+				+ "\nType 2 to do subtraction,"
+				+ "\nType 3 to do multiplication,"
+				+ "\nType 4 to do division,"
+				+ "\nType 5 to do comparisons between numbers:");
+		decision = input.nextInt();
 		
-		System.out.println("Please enter your second number: ");
-		double num2 = input.nextDouble();
-		// Asking the user for numbers that the program will use in following calculations
-		
-		double sum = num1 + num2;
-		
-		double difference = num1 - num2;
-		
-		double product = num1 * num2;
-		
-		double quotient = num1 / num2;
-		
-		if (num1 == num2) {
-			System.out.printf("%.2f is equal to %.2f.", num1, num2);
+		if (decision == 1) {
+			
+			double sum = 0;
+			
+			double num = -1;
+			
+			while (num != 0) {
+				
+				System.out.println("What is the number you would like to add? (Input 0 to end the addition):");
+				
+				num = input.nextInt();
+			
+				sum = sum + num;
+			}
+			System.out.printf("The sum of all of the inputted numbers is %.2f", sum);
 		}
-		else {
-			System.out.printf("%.2f is NOT equal to %.2f.", num1, num2);
-		}
-		if (num1 >= num2) {
-			System.out.printf("%n%.2f is greater than or equal to %.2f.", num1, num2);
-		}
-		else {
-			System.out.printf("%n%.2f is less than or equal to %.2f.", num1, num2);
-		}
-		System.out.printf("%nThe sum of %.2f and %.2f is %.2f.", num1, num2, sum);
-		
-		System.out.printf("%nThe difference between %.2f and %.2f is %.2f.", num1, num2, difference);
-		
-		System.out.printf("%nThe product of %.2f and %.2f is %.2f.", num1, num2, product);
-		
-		System.out.printf("%nThe quotient of %.2f and %.2f is %.2f.", num1, num2, quotient);
-		// Printing out statements for use in comparing the numbers that the user gave,
-		// as well as adding, subtracting, multiplying and dividing them. 
-		
-		
+		else if (decision == 2) {
+			
+			System.out.println("What is your starting number?");
+			
+			double diff = input.nextDouble();
+			
+			double num = -1;
+			
+			while (num != 0) {
+				
+				System.out.println("What is the  nextnumber you would like to add? (Input 0 to end the subtraction):");
+				
+				num = input.nextInt();
+			
+				diff = diff - num;
+			}
+			System.out.printf("The difference of all of the inputted numbers is %.2f", diff);
 		}
 		else if (decision == 3) {
 			
-		System.out.println("Please input the number you would like to do "
-					+ "the sum of n number formula for:");
-		int nnumber = input.nextInt();
+			System.out.println("What is your starting number?");
 			
-		int cnt = 1;
+			double pd =  input.nextDouble();
 			
-		int sum1 = 0;
+			double num = 0;
 			
-		while (cnt <= nnumber) {
+			while (num != 1) {
 				
-			sum1 = sum1 + cnt;
+				System.out.println("What is the next number you would like to multiply? (Input 1 to end the multiplication):");
+				
+				num = input.nextInt();
+			
+				pd = pd * num;
+			}
+			System.out.printf("The product of all of the inputted numbers is %.2f", pd);
+		}
+		else if (decision == 4) {
+			
+			System.out.println("What is your starting number?");
+			
+			double qt = input.nextDouble();
+			
+			double num = 0;
+			
+			while (num != 1) {
+				
+				System.out.println("What is the next number you would like to divide? (Input 1 to end the division):");
+				
+				num = input.nextInt();
+			
+				qt = qt / num;
+			}
+			System.out.printf("The quotient of all of the inputted numbers is %.2f", qt);
+		}
+		else if (decision == 5) {
+			
+			System.out.println("Please enter your first number: ");
+			double num1 = input.nextDouble();
+			
+			System.out.println("Please enter your second number: ");
+			double num2 = input.nextDouble();
+			
+			if (num1 == num2) {
+				System.out.printf("%.2f is equal to %.2f.", num1, num2);
+			}
+			else {
+				System.out.printf("%.2f is NOT equal to %.2f.", num1, num2);
+			}
+			if (num1 >= num2) {
+				System.out.printf("%n%.2f is greater than or equal to %.2f.", num1, num2);
+			}
+			else {
+				System.out.printf("%n%.2f is less than or equal to %.2f.", num1, num2);
+			}
+		}
+	}	
+		else if (decision == 3) {
+			
+		System.out.println("What is the maximum for the n natural numbers formula?:");
+				
+		int number = input.nextInt();
+		
+		System.out.println("What is the minimum for the n natural numbers formula?:");
+			
+		int cnt = input.nextInt();
+			
+		int sum = 0;
+			
+		while (cnt <= number) {
+				
+			sum = sum + cnt;
 			
 			cnt = cnt + 1;
 			
 		}
 		
-		System.out.println(sum1);
+		System.out.println(sum);
 		
 		}
 		else if (decision == 4) {
@@ -188,26 +251,28 @@ public class Calculator {
 			System.out.println("What is the final answer?:");
 			double answer = input.nextDouble();
 			
-			double logarithmanswer = Math.log(answer) / Math.log(base);
+			double la = Math.log(answer) / Math.log(base);
 			
-			System.out.printf("The logarithm of %.2f with base %.2f is %.2f.", answer, base, logarithmanswer);
+			// la is short for logarithm answer
+			
+			System.out.printf("The logarithm of %.2f with base %.2f is %.2f.", answer, base, la);
 		}
 		else if (decision == 3) {
 			System.out.println("What is the value of the variable a?:");
 			
-			double variablea = input.nextDouble();
+			double a = input.nextDouble();
 			
 			System.out.println("What is the value of the variable b?:");
 			
-			double variableb = input.nextDouble();
+			double b = input.nextDouble();
 			
-			System.out.println("What is the value of the exponent?:");
+			System.out.println("What is the answer you're looking for?:");
 			
-			double exponent = input.nextDouble();
+			double answer = input.nextDouble();
 			
-			double equation = variablea * Math.pow(variableb, exponent);
+			double equation = (Math.log(answer) / Math.log(b));
 			
-			System.out.printf("%nThe answer to %.2f * %.2f ^ %.2f is %.2f.", variablea, variableb, exponent, equation);
+			System.out.printf("%nThe answer to %.2f * %.2f ^ %.2f is %.2f.", a, b, equation, answer);
 		}
 	}
 		else if (decision == 8) {
@@ -280,9 +345,107 @@ public class Calculator {
 			
 			System.out.printf("%nThe cotangent of angle theta is %.2f", cotangent);
 		}
+		else if (decision == 10) {
+			System.out.println("Please input the variable a:");
+			
+			double a = input.nextDouble();
+			
+			System.out.println("Please input the variable b:");
+			
+			double b = input.nextDouble();
+			
+			System.out.println("Please input the variable c:");
+			
+			double c = input.nextDouble();
+			
+			double discriminant = Math.pow(b, 2) - 4 * a * c;
+			
+			if (discriminant < 0) {
+				System.out.println("There are no real roots");
+			}
+			else {
+			
+			double x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+			
+			double x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+			
+			System.out.printf("The roots of the quadratic equation are %.3f and %.3f", x1, x2);
+			}
+		}
+		else if (decision == 11) {
+			
+			System.out.println("Type 1 to do a fixed compounded interest formula,"
+					+ "\nType 2 to do a dynamic compounded interest formula");
+			
+			decision = input.nextInt();
+			
+			if (decision == 1) {
+			
+			System.out.println("What is the starting amount?:");
+			
+			double a = input.nextDouble();
+			
+			System.out.println("What is the interest %?: "
+					+ "(It probably should be an amount less than 1, "
+					+ "for example, if you had 10% interest then you'd put .1, etc.)");
+			
+			double r = input.nextDouble();
+			
+			System.out.println("How many cycles of interest is the money going through?:");
+			
+			double n = input.nextDouble();
+			
+			double answer = a * Math.pow(r + 1, n);
+			
+			System.out.printf("Your final amount after interest will be $%.2f.", answer);
+			}
+			else if (decision == 2) {
+				
+				System.out.println("What is your starting amount?:");
+				
+				double a = input.nextDouble();
+				
+				System.out.println("What is the first interest rate? (For ex. if it was 10% then you'd put .1, etc.");
+				
+				double r = input.nextDouble();
+				
+				System.out.println("How many interest cycles will this interest rate be active for?:");
+				
+				int n = input.nextInt();
+				
+				double fa = 0;
+				
+				System.out.println("How many more interest rates will you be going through?:");
+				
+				int ir = input.nextInt();
+				
+				fa = (a * Math.pow(1 + r, n));
+				
+				int cnt = 0;
+				while (cnt < ir) {
+					
+				System.out.printf("%nYour current amount after interest is $%.2f", fa);
+				
+				System.out.println("\nWhat is the next interest rate? (For ex. if it was 10% then you'd put .1, etc.");
+				
+				r = input.nextDouble();
+				
+				System.out.println("How many interest cycles will this interest rate be active for?:");
+				
+				n = input.nextInt();
+				
+				fa = (fa * Math.pow(1 + r, n));
+				
+				System.out.printf("%nYour final amount after interest is $%.2f.", fa);
+				
+				cnt++;
+				
+				}
+			}
+		}
 		System.out.println("\n\nPut the number 0 to restart the program,"
 				+ "\nPut any other number to stop the program.");
-		reset = input.nextInt();
+		reset = input.nextDouble();
 		}
 	}
 }
